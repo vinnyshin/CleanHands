19ca9832eae7ba008a7ed39322c2cddc218b6aff
Hojung, Shin <tankmom7279@gmail.com>
2021-06-01 19:53:15 +0900

    image view circle masked, title, frequency, extermination count stackView updated

--- CleanHands/CleanHands/CollectionView/CollectionDetailViewController.swift
+++ CleanHands/CleanHands/CollectionView/CollectionDetailViewController.swift
@@ -9,14 +9,43 @@ import UIKit
 
 class CollectionDetailViewController: UIViewController {
     
-    @IBOutlet weak var stackViewOutlet: UIView!
+    @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var stackViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var detailsStackView: UIStackView!
+    @IBOutlet weak var pathogenImageView: UIImageView!
     
-
+    @IBOutlet weak var pathogenKoreanNameLabel: UILabel!
+    @IBOutlet weak var pathogenScientificNameLabel: UILabel!
+    @IBOutlet weak var frequencyLabel: UILabel!
+    @IBOutlet weak var exterminationCountLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        animateDetails()
+//        animateDetails()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setPathogenImageUI()
+        setStackViewUI()
+    }
+    
+    func setPathogenImageUI() {
+        pathogenImageView.superview?.bringSubviewToFront(pathogenImageView)
+        
+        pathogenImageView.layer.masksToBounds = true
+        
+        pathogenImageView.layer.cornerRadius = pathogenImageView.bounds.width / 2
+        
+        pathogenImageView.layer.borderWidth = 5
+        pathogenImageView.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    func setStackViewUI() {
+        detailsStackView.layoutMargins = UIEdgeInsets(top: 100, left: 0, bottom: 100, right: 0)
+        detailsStackView.isLayoutMarginsRelativeArrangement = true
     }
     
 //    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -30,11 +59,11 @@ class CollectionDetailViewController: UIViewController {
 //        animateDetails()
 //    }
     
-    func animateDetails() {
-        self.stackViewTopConstraint.constant = 100
-        self.stackViewOutlet.setNeedsUpdateConstraints()
-        UIView.animate(withDuration: 1.0) {
-            self.view.layoutIfNeeded()
-        }
-    }
+//    func animateDetails() {
+//        self.stackViewTopConstraint.constant = 100
+//        self.stackViewOutlet.setNeedsUpdateConstraints()
+//        UIView.animate(withDuration: 1.0) {
+//            self.view.layoutIfNeeded()
+//        }
+//    }
 }
